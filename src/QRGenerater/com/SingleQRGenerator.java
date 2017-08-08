facb5719022434886172b34cae7eb3087651f4a2
shuang213 <shuang.han807@gmail.com>
2017-08-08 16:36:03 +0200

    SingleQRgenerator.java

---
+++ src/QRGenerater/com/SingleQRGenerator.java
@@ -0,0 +1,66 @@
+package QRGenerater.com;
+ 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
+ 
+import javax.imageio.ImageIO;
+ 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+public class SingleQRGenerator {
+	 
+	// Tutorial: http://zxing.github.io/zxing/apidocs/index.html
+ 
+	public static void main(String[] args) {
+		String myCodeText = "http://nystil.cn/";
+		String filePath = "/Users/wanderlust/Documents/generateQR.png";
+		int size = 250;
+		String fileType = "png";
+		File myFile = new File(filePath);
+		try {
+			
+			Map<EncodeHintType, Object> hintMap = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+			hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+			
+			// Now with zxing version 3.2.1 you could change border size (white border size to just 1)
+			hintMap.put(EncodeHintType.MARGIN, 1); /* default = 4 */
+			hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+ 
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size,
+					size, hintMap);
+			int SingleQRWidth = byteMatrix.getWidth();
+			BufferedImage image = new BufferedImage(SingleQRWidth, SingleQRWidth,
+					BufferedImage.TYPE_INT_RGB);
+			image.createGraphics();
+ 
+			Graphics2D graphics = (Graphics2D) image.getGraphics();
+			graphics.setColor(Color.WHITE);
+			graphics.fillRect(0, 0, SingleQRWidth, SingleQRWidth);
+			graphics.setColor(Color.BLACK);
+ 
+			for (int i = 0; i < SingleQRWidth; i++) {
+				for (int j = 0; j < SingleQRWidth; j++) {
+					if (byteMatrix.get(i, j)) {
+						graphics.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+			ImageIO.write(image, fileType, myFile);
+		} catch (WriterException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\n\nYou have successfully created QR Code.");
+	}
+}
